388578ff919836a89f79627a9dcab9df67353e22
Sean Chen-Landsberg <schenlandsberg@gmail.com>
2026-07-07 10:22:11 -0500

    22

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const lightbox = document.querySelector(".lightbox");
 const galleryImages = document.querySelectorAll(".gallery img");
+const popupImage = document.querySelector(".lightbox img");
 
 galleryImages.forEach(function(image) {
 
     image.addEventListener("click", function() {
         lightbox.style.display = "block";
-
+        popupImage.src = this.src;
+             
     });
 
 });
