4308720cca74fa4d1c2fe19e5afe288fd3900f43
Sean Chen-Landsberg <schenlandsberg@gmail.com>
2026-07-07 11:31:47 -0500

    22

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const lightbox = document.querySelector(".lightbox");
 const galleryImages = document.querySelectorAll(".gallery img");
 const popupImage = document.querySelector(".lightbox img");
-const closeButton = document.querySelector(".close");
+const thumbnails = document.querySelector(".thumbnails");
 
-galleryImages.forEach(function(image) {
+const closeButton = document.querySelector(".close");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+
+let currentIndex = 0;
+
+function showImage() {
+        popupImage.src = galleryImages[currentIndex].src;}
+
+galleryImages.forEach(function(image, index) {
 
     image.addEventListener("click", function() {
         lightbox.style.display = "flex";
-        popupImage.src = this.src;
-
+        currentIndex = index;
+        showImage();
     });
-    closeButton.addEventListener("click", function() {
+    
+    const thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumbnails.appendChild(thumb);
+
+    thumb.addEventListener("click", function() {
+        currentIndex = index;
+        showImage();
+    });
+});
+closeButton.addEventListener("click", function() {
         lightbox.style.display = "none";
     });
-
-});
+    nextButton.addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % galleryImages.length;
+        showImage();
+    });
+    prevButton.addEventListener("click", function() {
+        currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+        showImage();
+    });
